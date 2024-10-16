da66804917fb0df697e0800f230d33e66b699275
symond <yuuki.y.3035@gmail.com>
2024-10-16 19:50:30 +0900

    プロジェクト ファイルを追加します。

---
+++ SyncSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SyncSampleApp
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UpdateValue()
+        {
+            monitor.Content = int.Parse(parameter.Value.ToString());
+        }
+
+        private void ChangingParameter(object sender, RoutedEventArgs e)
+        {
+            Task.Run(() =>
+            {
+                this.Dispatcher.Invoke((Action)(() =>
+                {
+                    UpdateValue();
+                }));
+             });
+        }
+    }
+}
